c5dd9a46c5fd9d98f0d9ff8d6096a1f67ee5b66e
DJshenal <pererashenal2001@gmail.com>
2025-12-02 10:41:41 +0530

    Fix - dashboard

--- src/app/api/participant.js
+++ src/app/api/participant.js
@@ -2,24 +2,30 @@ import axios from "axios";
 import { API_BASE_URL } from "./url";
 
 const getAuthToken = () => {
-    const token = localStorage.getItem("token");
-    return `${token}`;
+  const token = localStorage.getItem("token");
+  return `${token}`;
 };
 
 export const registerParticipantWithPayment = async (eventId, data) =>
-    axios.post(`${API_BASE_URL}/tickets/${eventId}/register-with-payment`, { eventId, data }, {
-        headers: {
-            Authorization: `Bearer ${getAuthToken()}`,
-            'Content-Type': 'application/json',
-
-        },
-    });
+  axios.post(
+    `${API_BASE_URL}/tickets/${eventId}/register-with-payment`,
+    { eventId, data },
+    {
+      headers: {
+        Authorization: `Bearer ${getAuthToken()}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
 export const getEventParticipants = async (eventId = null) => {
-    const url = eventId
-        ? `${API_BASE_URL}/tickets/participants?eventId=${eventId}`
-        : `${API_BASE_URL}/tickets/participants`;
+  const url = eventId
+    ? `${API_BASE_URL}/tickets/participants?eventId=${eventId}`
+    : `${API_BASE_URL}/tickets/participants`;
 
-    return axios.get(url, {
-
+  return axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${getAuthToken()}`,
+    },
+  });
 };
